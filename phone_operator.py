511b371a048cbb4cd7153815b2fd8e3cabd5c378
plinkert <51674366+plinkert@users.noreply.github.com>
2019-06-18 20:18:51 +0200

    Update phone_operator.py
    Fix small bug

--- phone_operator.py
+++ phone_operator.py
@@ -5,10 +5,11 @@ import time
 import requests
 import re
 import xlrd
+import json
 from bs4 import BeautifulSoup
-import urllib3
+# import urllib3
 
-urllib3.disable_warnings()
+
 
 def is_phone_num(p: str) -> str:
     rePhone = re.compile(r'((48)\d{9})|\d{9}')
@@ -21,6 +22,7 @@ def is_phone_num(p: str) -> str:
 def mgsm(phone: str) -> str:
     try:
         url = 'https://www.mgsm.pl/pl/wjakiejsieci/'
+        requests.packages.urllib3.disable_warnings()
         r = requests.post(url, data = {'sCheckNumber': phone}, verify = False)
         soup = BeautifulSoup(r.text, 'html.parser')
         tab = soup('table', {'cellpadding':'2'})
@@ -41,28 +43,29 @@ def open_file_txt(file: str) -> list:
     return phones_list
 
 def create_report_txt(file: str, data: dict):
-    report = open(file, 'a')
-    for key, val in data:
-        report.write(key + " - " + val)
-    report.close()
+    with open(file, 'w') as report:
+        for key, val in data.items():
+            report.write(str(key)+ '\t' + str(val) + '\n')
 
 def run_scope(phones_list: list, report_name: str):
     phones = {}
     i = 0
     check = 0
     while(i < len(phones_list)):
-        operator = mgsm(phones_list[i])
-        print(i)
+        operator = mgsm(phones_list[i]).strip()
+        # print(str(i) + ' ' + operator)
         if operator != '':
             phones[phones_list[i]] = operator
             i+=1
         else:
-            time.sleep(60)
+            time.sleep(10)
             check += 1
             if check == 2:
-                phones[phones_list[i]] = 'Wrong number'
+                phones[phones_list[i]] = 'zły numer/brak operatora'
                 i+=1
                 check = 0
+
+    # print(phones)
     create_report_txt(report_name, phones)
 
 if __name__ == "__main__":
